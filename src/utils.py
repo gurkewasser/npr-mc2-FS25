6c9e6168b243b54c5532c3818597fd4b0e3acf72
gurkewasser <arian.iseni@students.fhnw.ch>
2025-05-28 13:05:01 +0000

    Add seed setting functionality in utils.py for reproducibility
    - Introduced a new function `set_seed()` to ensure consistent results across runs by setting seeds for random number generators in PyTorch, NumPy, and Python's random module.
    - Added a constant `SEED` for easy configuration of the seed value.
    - Imported necessary libraries: NumPy and random for seed management.

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,8 @@
 import torch
+import numpy as np
+import random
+
+SEED = 2277
 
 def find_max_batch_size(model, texts, start=4, max_possible=1024):
     """
@@ -23,3 +27,17 @@ def find_max_batch_size(model, texts, start=4, max_possible=1024):
             else:
                 raise e
     return best
+
+def set_seed():
+    torch.use_deterministic_algorithms(True)
+
+    random.seed(SEED)
+    np.random.seed(SEED)
+
+    torch.manual_seed(SEED)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(SEED)
+        torch.cuda.manual_seed_all(SEED)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
